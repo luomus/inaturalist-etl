1b62ec7b78a9abe83f7d276c48e45a102279a7f6
Mikko Heikkinen <hawk@biomi.org>
2026-01-30 12:58:28 +0200

    Set to run every 10 mins

--- app/getInat.py
+++ app/getInat.py
@@ -109,7 +109,7 @@ def getUpdatedGenerator(latestObsId, latestUpdateTime, pageLimit, perPage, sleep
       logger.log_full("No more observations.")
       git_sha = os.environ.get('APP_GIT_SHA', 'unknown')
       build_date = os.environ.get('APP_BUILD_DATE', 'unknown')
-      logger.log_full(f"iNaturalist ETL version: {git_sha} (built {build_date})")
+      logger.log_full(f"iNaturalist ETL version: {git_sha} (built {build_date} UTC)")
       logger.log_full("Total observations available on process start: " + str(totalObservationsAvailable))
       logger.log_full("Total observations processed: " + str(totalObservationsProcessed))
       yield False
